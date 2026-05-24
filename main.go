819d69f57a13a6d49fd554efd7a6012eca13ac90
Connie Caldera <166080234+c04o@users.noreply.github.com>
2026-05-24 12:38:31 -0600

    feat: boot up bubble tea framework
    tie up the previous logic from all files. view's string is passed here

--- main.go
+++ main.go
@@ -7,65 +7,17 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-// holds the program state
-type model struct {
-	targetText string
-	userInput  string
-}
-
-// test text
-func initialModel() model {
-	return model{
-		targetText: "the quick brown fox jumps over the lazy dog",
-		// init blank
-		userInput: "",
-	}
-}
-
-// init i/o (like timers)
-func (m model) Init() tea.Cmd {
-	return nil
-}
-
-// handle events (key presses, etc.)
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-
-		// quit
-		case "ctrl+c", "esc":
-			return m, tea.Quit
-
-		// if backspace
-		case "backspace":
-			if len(m.userInput) > 0 {
-				// reduce input
-				m.userInput = m.userInput[:len(m.userInput)-1]
-			}
-		default:
-			// handle standard character keys for now
-			if len(msg.String()) == 1 {
-				m.userInput += msg.String()
-			}
-		}
-	}
-	return m, nil
-}
-
-// render ui
-func (m model) View() string {
-	s := "Type the following:\n\n"
-	s += m.targetText + "\n\n"
-	s += "Your input: " + m.userInput + "\n\n"
-	s += "(press esc to quit)"
-	return s
-}
-
+// main entry point of app
 func main() {
-	p := tea.NewProgram(initialModel())
+	// create a new bubble tea program using initial model state
+	// use the alt screen so it cleans up the terminal when we quit
+	p := tea.NewProgram(initialModel(), tea.WithAltScreen())
+
+	// run the program and check if any fatal errors occurred
 	if _, err := p.Run(); err != nil {
-		fmt.Printf("Alas, there's been an error: %v", err)
+		// print error if failed
+		fmt.Printf("error: %v", err)
+		// exit with a non-zero status code to indicate failure
 		os.Exit(1)
 	}
 }
